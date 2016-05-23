050896af5c4855c2b3457e696f2c711376c67423
Kandice McLean <mcleankand@jeff-cafe.ecs.vuw.ac.nz>
2016-05-23 13:53:32 +1200

    fixed the drop down menu and fixed the index in all .html files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3030;
 var pg = require('pg');
 
 
