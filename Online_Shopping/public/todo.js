1448981fcefdb359c0f8245ecad503bfad10e5ad
Kandice McLean <mcleankand@olive-olive.ecs.vuw.ac.nz>
2016-05-23 15:45:48 +1200

    adding code to connect to db and get item table

--- Online_Shopping/public/todo.js
+++ Online_Shopping/public/todo.js
@@ -4,7 +4,7 @@ var ERROR_LOG = console.error.bind(console);
 
 //GET recieves the information from the items table in database, no changes are made
 $.ajax({
-    url: "http://localhost:5000/items",
+    url: "https://evening-cove-32171.herokuapp.com/items",
     type: "GET",
     }).then(redraw, ERROR_LOG);
 
