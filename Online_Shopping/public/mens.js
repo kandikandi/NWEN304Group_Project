93d60b63baad69ce2e8164a1b8f845af08b85dcf
Kandice McLean <mcleankand@pravda.ecs.vuw.ac.nz>
2016-05-25 15:08:50 +1200

    peer programming, added .js files for each html page and added a get to the catalogue items.js

---
+++ Online_Shopping/public/mens.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(e) {
+
+var ERROR_LOG = console.error.bind(console);
+var ip = 'https://evening-cove-32171.herokuapp.com'; // app address
+
+//GET recieves the information from the items table in database, no changes are made
+$.ajax({
+    url: ip+'/catalogue/mens',
+    type: "GET",
+    }).then(redraw, ERROR_LOG);
+
+function redraw(data){
+    console.log('redrawing', data);
+    };  
--- Online_Shopping/public/womens.js
+++ Online_Shopping/public/womens.js
@@ -2,30 +2,13 @@
 
 var ERROR_LOG = console.error.bind(console);
 var ip = 'https://evening-cove-32171.herokuapp.com'; // app address
+
 //GET recieves the information from the items table in database, no changes are made
 $.ajax({
-    url: ip+'/items',
+    url: ip+'/catalogue/womens',
     type: "GET",
     }).then(redraw, ERROR_LOG);
 
 function redraw(data){
     console.log('redrawing', data);
-    };
-
-
-
-	/*function registerUser(userName, email, password){
-		$.ajax({
-			method: 'PUT',
-			url: ip+'/register',
-			data: JSON.stringify({
-				username: userName
-				email: email
-				password: password
-			}),
-			contentType: "application/json",
-			dataType: "json"
-		}).then(ERROR_LOG);
-	};*/
-
-});
+    };  
