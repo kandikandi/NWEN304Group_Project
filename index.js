7daefdfeba48e86103e6e33e0fc3822e8af7d6cc
Kandice McLean <mcleankand@prefab.ecs.vuw.ac.nz>
2016-05-27 13:42:26 +1200

    Adding database queries for login, peer programming

--- index.js
+++ index.js
@@ -137,17 +137,17 @@ app.get('/catalogue/womens', function(request, response){
 //GET ALL CHILDREN ITEMS
 app.get('/catalogue/kids', function(request, response){
 var query = client.query("SELECT * FROM users WHERE username = 'hi' AND password = 'k'"); 
-if(query = ""){
+if(query = null){
 console.log("i am a fish");
 } 
   //  var query = client.query("SELECT * FROM items WHERE cat_id = 0");
-  var results = [];
-  query.on('row', function(row){
-    results.push(row);
-  });
-  query.on('end', function(){
-    response.json(results);
-  });
+ // var results = [];
+ // query.on('row', function(row){
+  //  results.push(row);
+ // });
+  //query.on('end', function(){
+   // response.json(results);
+ // });
 });
 
 
