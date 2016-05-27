f0cd73355bfb66a627e88ef8e9470a325262113c
Kandice McLean <mcleankand@olive-olive.ecs.vuw.ac.nz>
2016-05-27 12:44:40 +1200

    trying to get fb auth working peer programming

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ var pg = require('pg');
 var http = require('http');
 var passport = require('passport');
 var util = require('util');
-var HerokuStrategy = require('passport-heroku').Strategy;
-
-var HEROKU_CLIENT_ID = 123456; //process.env.HEROKU_CLIENT_ID;
-var HEROKU_CLIENT_SECRET = 'abc123';//process.env.HEROKU_CLIENT_SECRET;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 /*set up passport*/
 passport.serializeUser(function(user,done){
@@ -20,33 +17,27 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-passport.use(new HerokuStrategy({
-    clientID: HEROKU_CLIENT_ID,
-    clientSecret: HEROKU_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/heroku/callback"
+
+passport.use(new FacebookStrategy({
+    clientID: '236128690099176',
+    clientSecret: 'c522eb05e7a97cd5e68739655df582c0',
+    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
+  function(token, refreshToken, profile, done) {
    
     process.nextTick(function () {
-        
+    
       return done(null, profile);
     });
   }
 ));
 
-//app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  //app.use(express.logger());
-//  app.use(express.cookieParser());
-//  app.use(express.bodyParser());
-  //app.use(express.methodOverride());
-//  app.use(express.session({ secret: 'keyboard cat' }));
   app.use(passport.initialize());
   app.use(passport.session());
- // app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-//});
+
 
 /*For defaulting back to https*/
 app.get('*',function(req,res,next){
@@ -59,11 +50,11 @@ app.get('*',function(req,res,next){
 /*default to ssl connections so heroku will allow us to use the apps database*/
 pg.defaults.ssl = true;
 
-/*var client = new pg.Client(process.env.DATABASE_URL);
+var client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 /*Connect to the apps pg database on heroku*/
-/*pg.connect(process.env.DATABASE_URL,function(err,client){
+pg.connect(process.env.DATABASE_URL,function(err,client){
     if(err) throw err;
     console.log('Connected to postgres, getting schemas...');
 
@@ -71,7 +62,7 @@ client.connect();
     .on('row', function(row) {
         console.log(JSON.stringify(row));
     });
-});*/
+});
 
 //just testing
 var bodyParser = require('body-parser');
@@ -158,7 +149,7 @@ app.get('/catalogue/kids', function(request, response){
 
 //LOGIN
 app.get('/login',ensureAuthenticated, function(request, response){
-   res.render('login',{user: req.user});
+   res.render('login',{user: req.user});   
 });
 
 //REGISTER
@@ -169,23 +160,21 @@ app.put('/register', function(req, res){
 	// res.send("Created\n");
 });
 
-app.get('/auth/heroku',
-  passport.authenticate('heroku'),
-  function(req, res){
-    // The request will be redirected to Heroku for authentication, so this
-    // function will not be called.
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){   
   });
 
-app.get('/auth/heroku/callback', 
-        passport.authenticate('heroku', { failureRedirect: '/login' }),
+app.get('/auth/facebook/callback', 
+        passport.authenticate('facebook', { failureRedirect: '/login' }),
         function(req, res) {
-          res.redirect('/');
+         return res.redirect('/');
         });
 
-/*app.get('/logout', function(req, res){
+app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
-});*/
+});
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
@@ -193,19 +182,6 @@ function ensureAuthenticated(req, res, next) {
 }
 
 
-// //UPDATE TASK
-// app.post('/update',function(req, res){
-// 	console.log('Updating...\n');
-// 	var query = client.query("UPDATE todo SET isDone = NOT isDone WHERE task = '" + req.body.task + "'");
-// 	res.send("Updated\n");
-// });
-
-// //DELETE TASK
-// app.delete('/del', function(req, res){
-// 	console.log('Deleting...\n');
-// 	var query = client.query("DELETE FROM todo WHERE task = '" + req.body.task +"'");
-// 	res.send("Deleted\n");
-// });
 
 //Add headers
 app.use(function(req, res, next){
@@ -222,7 +198,4 @@ app.listen(port, function () {
 
 
 //TEST CASES
-// GET: curl -H "Content-Type: application/json" -X GET 130.195.4.166:8080/all_tasks
-// CREATE: curl -H "Content-Type: application/json" -X PUT -d '{"task":"test task"}' 130.195.4.166:8080/create
-// UPDATE: curl -H "Content-Type: application/json" -X POST -d '{"task":"test task", "isDone":"true"}' 130.195.4.166:8080/update
-// REMOVE: curl -H "Content-Type: application/json" -X DELETE -d '{"task":"hello"}' 130.195.4.166:8080/del
+
