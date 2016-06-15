3f7cb3971b8d97caf15702f5afa51e0e2f88bf89
Kandice McLean <mcleankand@cafe-india.ecs.vuw.ac.nz>
2016-06-16 10:41:36 +1200

    trying to fix timeout issue with PUT and POST requests

--- index.js
+++ index.js
@@ -266,8 +266,10 @@ app.get('/auth', function(req, res, next){
 //PROFILE
 app.get('/profile', function(req, res){
 
-console.log(req.user.username);
-var query = client.query("SELECT * FROM users WHERE username = '"+ req.user.username + "';");
+var str = req.user.username;
+var user = str.slice(1, -1);
+console.log(user);
+var query = client.query("SELECT * FROM users WHERE username = '"+ user + "';");
 var results = [];
 
   query.on('row', function(row){
