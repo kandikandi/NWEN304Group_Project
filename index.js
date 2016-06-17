8f26ecf6af801e03cf2328e0791668432eb39e8b
Kandice McLean <mcleankand@cafe-india.ecs.vuw.ac.nz>
2016-06-17 14:34:38 +1200

    fixing local strategy

--- index.js
+++ index.js
@@ -162,8 +162,7 @@ passport.use('facebook', new FacebookStrategy({
 ));
 
 app.get('/auth/facebook', 
-    passport.authenticate('facebook',{ scope: 'email'}),   
-);
+    passport.authenticate('facebook',{ scope: 'email'}));
 
 app.get('/auth/facebook/callback', 
         passport.authenticate('facebook', {  
