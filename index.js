3025769bb6da63765dfff774342a4760e16e0663
Kandice McLean <mcleankand@cafe-india.ecs.vuw.ac.nz>
2016-06-27 14:27:15 +1200

    adding price to success

--- index.js
+++ index.js
@@ -456,6 +456,7 @@ app.post('/cart/buy', function(req,res){
           console.log("ITEM PRICE IS COMING THROUGH AS: " + row.item_price);
           console.log("ROW ITEM ID: " +row.item_id);
           console.log("TOTAL COOKIE = " + req.session.total);
+          console.log("Username : " + req.session.username);
         });
         
         query = client.query("INSERT INTO purchases (orders, username) VALUES ($1, $2)",[products, req.session.username]);
