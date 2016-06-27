896ef2e0dbeddfeb99794cdaa801fe66cf1a17f5
Kandice McLean <mcleankand@cafe-india.ecs.vuw.ac.nz>
2016-06-27 14:24:59 +1200

    adding price to success

--- index.js
+++ index.js
@@ -454,6 +454,7 @@ app.post('/cart/buy', function(req,res){
           req.session.total = req.session.total + row.item_price;
           products.push(row.item_id);
           console.log("ITEM PRICE IS COMING THROUGH AS: " + row.item_price);
+          console.log("ROW ITEM ID: " +row.item_id);
           console.log("TOTAL COOKIE = " + req.session.total);
         });
         
